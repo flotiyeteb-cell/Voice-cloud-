0f6b4a321455f4c20ea73198ccdab369e2b271bb
flotiyeteb-cell <flotiyeteb@gmail.com>
2026-05-04 23:10:42 +0200

    Create BuildConfig.kt

---
+++ app/src/main/java/com/voiceassistant/pro/BuildConfig.kt
@@ -0,0 +1,9 @@
+package com.voiceassistant.pro
+
+object BuildConfig {
+    const val DEBUG = true
+    const val APPLICATION_ID = "com.voiceassistant.pro"
+    const val BUILD_TYPE = "debug"
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "2.0.0-PRO"
+}
